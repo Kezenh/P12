0c9ff026faf0c488b64e6475b1884a2a7f005089
Quentin Rampteau <quentin425@hotmail.fr>
2022-06-21 18:10:59 +0200

    NEW : Daily activity chart

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, PureComponent } from "react"
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 import fetchUserDatas from "../services/user"
 import fetchActivityDatas from "../services/activity"
 import fetchAverageSessionsDatas from "../services/average"
@@ -11,6 +12,7 @@ import apple from "../assets/apple.png"
 import burger from "../assets/burger.png"
 
 function Profile() {
+
     const userId = 18
     const [firstName, setFirstName] = useState("")
     const [score, setScore] = useState("")
@@ -34,7 +36,12 @@ function Profile() {
     }
 
     async function setActivity() {
-        setSessions(await fetchActivityDatas(userId))
+        let sessions = await fetchActivityDatas(userId)
+        sessions.map((session, index) => {
+            session.kcal = session.calories / 10
+            session.index = index + 1
+        })
+        setSessions(sessions)
     }
 
     async function setAverageSessions() {
@@ -45,6 +52,17 @@ function Profile() {
         const performanceDatas = await fetchPerformanceDatas(userId)
         setActivitiesKind(performanceDatas.kind)
         setActivitiesValues(performanceDatas.data)
+    }
+
+    function customTooltip({ payload }) {
+        if (payload && payload.length) {
+            return (
+                <div className="customTooltip">
+                    <p className="customTooltipText">{`${payload[0].value}kg`}</p>
+                    <p className="customTooltipText">{`${payload[1].value}Kcal`}</p>
+                </div>
+            )
+        }
     }
 
     useEffect(() => {
@@ -68,16 +86,36 @@ function Profile() {
                 <div className="datas">
                     <div className="graphs">
                         <div className="dailyActivity">
-                            {sessions.map((session, index) => {
-                                return (
-                                    <div key={index}>
-                                        <p>{`Session ${index + 1} :`}</p>
-                                        <p>{`Day : ${session.day}`}</p>
-                                        <p>{`Kilogrammes : ${session.kilogram}`}</p>
-                                        <p>{`Calories : ${session.calories}`}</p>
-                                    </div>
-                                )
-                            })}
+                            <div className="dailyActivityLegend">
+                                <p>Activité quotidienne</p>
+                                <ul>
+                                    <li className="legend">
+                                        <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path fillRule="evenodd" clipRule="evenodd" d="M4 8C6.20914 8 8 6.20914 8 4C8 1.79086 6.20914 0 4 0C1.79086 0 0 1.79086 0 4C0 6.20914 1.79086 8 4 8Z" fill="#282D30" />
+                                        </svg>
+                                        <p className="legendText">Poids (kg)</p>
+                                    </li>
+                                    <li className="legend">
+                                        <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path fillRule="evenodd" clipRule="evenodd" d="M4 8C6.20914 8 8 6.20914 8 4C8 1.79086 6.20914 0 4 0C1.79086 0 0 1.79086 0 4C0 6.20914 1.79086 8 4 8Z" fill="#E60000" />
+                                        </svg>
+                                        <p className="legendText">Calories brûlées (kCal)</p>
+                                    </li>
+                                </ul>
+                            </div>
+                            <BarChart
+                                width={835}
+                                height={202}
+                                data={sessions}
+                                barGap={8}
+                            >
+                                <CartesianGrid strokeDasharray="2" vertical={false} />
+                                <XAxis dataKey="index" tickLine={false} tickMargin={16} tick={{ fill: '#9B9EAC' }} axisLine={false} />
+                                <YAxis orientation="right" domain={['dataMin - 2', 'dataMax + 2']} tickLine={false} axisLine={false} tick={{ fill: '#9B9EAC' }} />
+                                <Tooltip content={customTooltip} />
+                                <Bar dataKey="kilogram" fill="#282D30" barSize={7} radius={[3, 3, 0, 0]} />
+                                <Bar dataKey="kcal" fill="#E60000" barSize={7} radius={[3, 3, 0, 0]} />
+                            </BarChart>
                         </div>
                         <div className="smallGraphs">
                             <div className="sessionAverage">
